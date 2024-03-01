a2d5c5afb3faf4740226150bb0b7596f5ef8dbc5
AnggitaSaputra <anggitasaputra682@gmail.com>
2024-03-01 19:09:53 +0700

    loginregister

--- resources/views/page/auth/login.blade.php
+++ resources/views/page/auth/login.blade.php
@@ -1,24 +1,30 @@
 @extends('layout.app')
 
 @section('content')
-   <form id="formLogin">
-        @csrf
-        <input type="email" name="email" id="email" placeholder="masukkan email">
-        <input type="password" name="password" id="password" placeholder="masukkan password">
-        <button type="button" onclick="loginUser()">Login</button>
-    </form>
-    <a href="{{ route('register') }}">Register</a>
-    @if(session('error'))
-                    <div class="rounded-lg p-2 w-full bg-red-500 text-white flex justify-between">
-                        <p>
-                            {{ session('error') }}
-                        </p>
-                        <button type="button" class="text-white" onclick="this.parentElement.style.display='none'">
-                            <span>&times;</span>
-                        </button>
-                    </div>
-                @endif
+    <div class="flex justify-center items-center h-screen bg-gray-100">
+        <div class="max-w-md bg-white p-8 rounded-lg shadow-md">
+            <form id="formLogin" class="mb-4">
+                @csrf
+                <p class="text-center text-gray-600 text-2xl mb-4">Selamat Datang</p>
+                <p class="text-center text-gray-600 mb-6">Login menggunakan akun anda</p>
+                <input type="email" name="email" id="email" placeholder="Email" class="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+                <input type="password" name="password" id="password" placeholder="Password" class="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+                <button type="button" onclick="loginUser()" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Login</button>
+            </form>
+            <p class="text-center text-gray-600 mt-4">Belum punya akun? <a href="{{ route('register') }}" class="text-blue-500 hover:text-blue-600">Daftar</a></p>
+            @if(session('error'))
+                <div class="rounded-lg p-2 bg-red-500 text-white flex justify-between items-center max-w-md mt-4">
+                    <p>{{ session('error') }}</p>
+                    <button type="button" onclick="this.parentElement.style.display='none'" class="text-white">
+                        <span>&times;</span>
+                    </button>
+                </div>
+            @endif
+        </div>
+    </div>
 @endsection
+
+
 @section('script')
     <script>
         function loginUser() {
--- resources/views/page/auth/register.blade.php
+++ resources/views/page/auth/register.blade.php
@@ -1,19 +1,24 @@
 @extends('layout.app')
 
 @section('content')
-    <form id="formRegister">
-        @csrf
-        <input type="text" name="nama" id="nama" placeholder="masukkan nama">
-        <input type="email" name="email" id="email" placeholder="masukkan email">
-        <select name="role" id="role">
-            <option value="official">Official</option>
-            <option value="manager">Manager</option>
-        </select>
-        <input type="password" name="password" id="password" placeholder="masukkan password">
-        <input type="password" name="password_konfirmasi" id="password_konfirmasi" placeholder="ketik ulang password">
-        <button type="button" onclick="registerUser()">Register</button>
-    </form>
-    <a href="{{ route('login') }}">Login</a>
+    <div class="flex justify-center items-center h-screen bg-gray-100">
+        <div class="max-w-md bg-white p-8 rounded-lg shadow-md">
+        <form id="formRegister" class="mb-4">
+            @csrf
+            <p class="text-center text-gray-600 text-2xl mb-4">Selamat Datang</p>
+            <p class="text-center text-gray-600 mb-6">Silahkan melakukan pendaftaran</p>
+            <input type="text" name="nama" id="nama" placeholder="masukkan nama" class="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+            <input type="email" name="email" id="email" placeholder="masukkan email" class="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+            <select name="role" id="role" class="px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400 appearance-none">
+                <option value="official" class="py-2">Official</option>
+                <option value="manager" class="py-2">Manager</option>
+            </select>
+            <input type="password" name="password" id="password" placeholder="masukkan password"class=" w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+            <input type="password" name="password_konfirmasi" id="password_konfirmasi" placeholder="ketik ulang password" class="w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+            <button type="button" onclick="registerUser()" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Register</button>
+        </form>
+        <p class="text-center text-gray-600 mt-4">Sudah punya akun? <a href="{{ route('login') }}" class="text-blue-500 hover:text-blue-600">Login</a></p>
+    </div>
 @endsection
 @section('script')
     <script>
